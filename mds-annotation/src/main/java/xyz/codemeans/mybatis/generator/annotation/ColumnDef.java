f7cc2cc63fd630403381e16b710b350f18724629
yuan <yuan@preferr.com>
2021-09-17 16:05:35 +0800

    ColumnDef

---
+++ mds-annotation/src/main/java/xyz/codemeans/mybatis/generator/annotation/ColumnDef.java
@@ -0,0 +1,26 @@
+package xyz.codemeans.mybatis.generator.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.sql.JDBCType;
+
+/**
+ * specify name, jdbcType of column if needed. Or, name will be generated automatically and jdbcType
+ * will be derived by the type of annotated field.
+ *
+ * @author: yuanwq
+ * @date: 2021/9/17
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ColumnDef {
+
+  /**
+   * specify column name.
+   */
+  String name() default "";
+
+  JDBCType jdbcType();
+}
