57e0fdbbfbc9f498cc220cb1d88a01ca0d56ef79
yuan <yuan@preferr.com>
2021-09-17 16:06:50 +0800

    SqlIgnore

---
+++ mds-annotation/src/main/java/xyz/codemeans/mybatis/generator/annotation/SqlIgnore.java
@@ -0,0 +1,17 @@
+package xyz.codemeans.mybatis.generator.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * ignore generating the annotated type or field
+ *
+ * @author yuanwq
+ */
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface SqlIgnore {
+
+}
