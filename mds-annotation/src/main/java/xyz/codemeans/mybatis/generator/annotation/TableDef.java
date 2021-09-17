a190cf772b23394f54af5846a17cd6ebc9b64f56
yuan <yuan@preferr.com>
2021-09-17 15:49:04 +0800

    TableDef

---
+++ mds-annotation/src/main/java/xyz/codemeans/mybatis/generator/annotation/TableDef.java
@@ -0,0 +1,32 @@
+package xyz.codemeans.mybatis.generator.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specify catalog, schema, table of the annotated type if needed. Or table name will be generated
+ * automatically according to config.
+ *
+ * @author yuanwq
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface TableDef {
+
+  /**
+   * table name, like: "my_table", "my_db.my_table"
+   */
+  String table() default "";
+
+  /**
+   * schema name: prepended to table name if present
+   */
+  String schema() default "";
+
+  /**
+   * catalog name: prepended to table name if present
+   */
+  String catalog() default "";
+}
